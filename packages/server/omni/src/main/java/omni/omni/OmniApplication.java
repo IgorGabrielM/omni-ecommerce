81ef1613e3bb7084f2a5bd93a4f4715b7efc92d8
Diogo-Santoro <diogo.santoro05@gmail.com>
2022-07-25 20:18:38 -0300

    First Commit omni server

---
+++ packages/server/omni/src/main/java/omni/omni/OmniApplication.java
@@ -0,0 +1,14 @@
+package omni.omni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniApplication.class, args);
+	}
+
+
+}
