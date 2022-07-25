29319bbd777754982a18e716971973816fceed23
Diogo-Santoro <diogo.santoro05@gmail.com>
2022-07-25 20:22:52 -0300

    First Commit omni server

--- packages/server/omni/src/main/java/omni/omni/repositories/UserRepository.java
+++ packages/server/omni/src/main/java/omni/omni/repositories/UserRepository.java
@@ -3,9 +3,6 @@ package omni.omni.repositories;
 import omni.omni.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-
-
 @Repository
 
 public interface UserRepository extends JpaRepository<User, String> {
