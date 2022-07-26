8778be4078aaacde05ecde88246f6482e06b7c18
IgorGabrielM <igorgabprofissional@gmail.com>
2022-07-25 22:32:25 -0300

    add a simple changes

--- packages/mobile/pages/Login.tsx
+++ packages/mobile/pages/Login.tsx
@@ -1,18 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { VStack } from "native-base";
-import { Header } from '../components/Header';
+import { useState } from 'react';
+import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Envelope, Key } from 'phosphor-react-native'
 import {Input} from '../components/Input'
 import {Button} from '../components/Button'
 
 export function Login(){
-    return(
-        <VStack flex={1} p={6} bg='gray.600'>
-            <Header title="Nova solicitação" /> 
 
-            <Input placeholder="Número do patrimonio" mt={4} onChangeText={setPatrimony}/>            
-            <Input placeholder="Descrição do problema" mt={5} flex={1} multiline textAlignVertical="top" onChangeText={setDescription}/>
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState('');
+    const {colors} = useTheme()
+
+    return(
+        <VStack flex={1} alignItems='center' bg='gray.600' px={8} pt={24}>
         
-            <Button title="Cadastrar" mt={5} isLoading={isLoading} onPress={handleNewOrderRegister}/>
-        </VStack>
+        <Heading color='gray.100' fontSize='xl' mt={20} mb={6}>
+            Acesse sua conta
+        </Heading>
+
+        <Input
+        InputLeftElement={<Icon as={ <Envelope color={colors.gray[300]}/>} ml={4}/>}
+        mb={5} 
+        placeholder="E-mail"
+        onChangeText={setEmail}
+        />
+        
+        <Input  mb={8} placeholder="Senha"
+        InputLeftElement={<Icon as={ <Key color={colors.gray[300]}/>} ml={4}/>}
+        secureTextEntry
+        onChangeText={setPassword}/>
+        <Button title='Entrar' w='full' onPress={handleSignIn} isLoading={isLoading}/>
+
+
+    </VStack>
     )
 }
